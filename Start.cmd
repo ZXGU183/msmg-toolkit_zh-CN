37d91a278c15ee792bc9d37306696844b4ef9ecd
ZXGU183 <53319390+ZXGU183@users.noreply.github.com>
2025-08-24 11:15:32 +0800

    feat(Start.cmd, Toolkit.cmd): 检测系统语言并启动对应的主程序；Toolkit.cmd改回中文版并保持原版的缩进风格
    目前仅支持简体中文系统，其他语言的系统环境则使用英文版本

--- Start.cmd
+++ Start.cmd
@@ -1,7 +1,19 @@
 @echo off
 
+setlocal
+
 if "%PROCESSOR_ARCHITECTURE%" equ "x86" set "Arch=x86"
 if "%PROCESSOR_ARCHITECTURE%" equ "AMD64" set "Arch=x64"
 
-@start /b "ToolKit - TrustedInstaller" "%~dp0Bin\%Arch%\NSudo.exe" -U:T -P:E "%~dp0ToolKit.cmd"
+set "ToolKitCmd=ToolKit_en.cmd"
 
+for /f "tokens=6 delims= " %%o in ('DISM /Online /English /Get-Intl ^| findstr /i /C:"Default system UI language"') do (set "HostUILanguage=%%o")
+
+if "%HostUILanguage%" NEQ "zh-CN" set "ToolKitCmd=ToolKit.cmd"
+
+echo Using ToolKit: %ToolKitCmd%
+
+start /b "ToolKit - TrustedInstaller" "%~dp0Bin\%Arch%\NSudo.exe" -U:T -P:E "%~dp0%ToolKitCmd%"
+
+endlocal
+exit
